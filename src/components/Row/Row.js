158460d7fbdbc3228fc384ecb4eff5a9bc37ac2e
chen.dong <chen_dong85@foxmail.com>
2017-04-30 22:44:39 +0800

    only make the related row rendering

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,18 +1,32 @@
 import React, {Component, PropTypes} from 'react';
 import Cell from '../Cell/Cell'
 import './Row.css'
-const Row = ({rowId, cells, rat}) => {
-  return (
-    <div className="row">
-      {cells.map((cell, index) => {
-        if (rowId === rat.row && index === rat.column) {
-          return <Cell key={cell.id} borders={cell} rat/>
-        }
-        return <Cell key={cell.id} borders={cell}/>
-      })}
-    </div>
-  )
-};
+
+class Row extends Component {
+  
+  shouldComponentUpdate(nextProps) {
+    const rowId = nextProps.rowId;
+    const nextRat = nextProps.rat;
+    const currentRat = this.props.rat;
+    
+    return rowId === nextRat.row || rowId === currentRat.row
+    
+  }
+  
+  render(){
+    const {rowId, cells, rat} = this.props;
+    return (
+      <div className="row">
+        {cells.map((cell, index) => {
+          if (rowId === rat.row && index === rat.column) {
+            return <Cell key={cell.id} borders={cell} rat/>
+          }
+          return <Cell key={cell.id} borders={cell}/>
+        })}
+      </div>
+    )
+  }
+}
 
 Row.propTypes = {
   rowId: PropTypes.number.isRequired,
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,6 @@ import locationHelper from '../helpers/locationHelper';
 const rat = (state = {row:0, column: 0}, action) => {
   switch (action.type){
     case 'MOVE_RAT':
-      console.log(locationHelper.generateLocation(state, action.panel, action.direction))
       return locationHelper.generateLocation(state, action.panel, action.direction);
     default:
       return state
