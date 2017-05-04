b8e10a23c1755998f676f57ae9c8a998e1c016fe
chen.dong <chen_dong85@foxmail.com>
2017-05-04 23:52:33 +0800

    remove no used action

--- src/actions/panel.js
+++ src/actions/panel.js
@@ -1,12 +1,3 @@
-export const moveRat = direction => (dispatch, getState) => {
-  const { panel } = getState();
-  dispatch({
-    type: 'MOVE_RAT',
-    direction,
-    panel
-  })
-};
-
 export const autoRat = (location) => ({
     type:'AUTO_RAT',
     location
