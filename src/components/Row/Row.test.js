4f05c31e82f7195275534cb0db2cb4abd0833316
chen.dong <chen_dong85@foxmail.com>
2017-05-04 22:03:45 +0800

    modify the test for row

--- src/components/Row/Row.test.js
+++ src/components/Row/Row.test.js
@@ -5,26 +5,32 @@ import Cell from '../Cell/Cell';
 
 describe('Row', ()=> {
   const cells= [
-    {top:true, right:true, bottom:false, left:false, id:'00'},
-    {top:true, right:true, bottom:true, left:true, id:'01'},
+    {top:true, right:true, bottom:false, left:false},
+    {top:true, right:true, bottom:true, left:true},
+    {top:true, right:true, bottom:true, left:true},
   ];
   
   const rowId = 0;
   
-  const rat = {
+  const rats = [{
     row:0,
     column:1
-  };
+  }, {
+    row:0,
+    column:2
+  }];
   
   it('should render Row component with correct border', () => {
-    const rowWarpper = shallow(<Row rowId={rowId} cells={cells} rat={rat}/>);
-    expect(rowWarpper.find(Cell).length).toEqual(2);
+    const rowWarpper = shallow(<Row rowId={rowId} cells={cells} rats={rats}/>);
+    expect(rowWarpper.find(Cell).length).toEqual(3);
   });
   
   it('should pass the rat to right cell is rat passed in ', () => {
-    const rowWarpper = shallow(<Row rowId={rowId} cells={cells}  rat={rat} />);
+    const rowWarpper = shallow(<Row rowId={rowId} cells={cells}  rats={rats} />);
     
-    const ratCell = rowWarpper.find(Cell).filter('[rat]');
-    expect(ratCell.key()).toEqual('01');
+    const ratCells = rowWarpper.find(Cell).filter('[rat]');
+    expect(ratCells.nodes[0].key).toEqual('01');
+    expect(ratCells.length).toEqual(2);
+    expect(ratCells.nodes[1].key).toEqual('02');
   })
 });
